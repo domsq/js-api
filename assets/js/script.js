d4f42f8789792e5ffbffd2beacbd17456c325e43
domsq <Domsq@hotmail.com>
2021-10-06 14:53:34 +0000

    Add code to display exceptions in modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,39 +5,6 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
-async function getStatus(e) {
-    const queryString = `${API_URL}?api_key=${API_KEY}`;
-
-    const response = await fetch(queryString);
-
-    const data = await response.json();
-
-    if (response.ok) {
-        console.log(data.expiry);
-    };
-};
-
-async function postForm(e) {
-    const form = new FormData(document.getElementById("checksform"));
-
-    const response = await fetch(API_URL, {
-        method: "POST",
-        headers: {
-            "Authorization": API_KEY,
-        },
-        body: form
-    });
-
-    const data = await response.json();
-
-    if (response.ok) {
-        displayErrors(data);
-    }
-    else {
-        throw new Error(data.error);
-    };
-}
-
 function displayErrors(data) {
     let heading = `JSHint Results for ${data.file}`;
 
@@ -56,5 +23,70 @@ function displayErrors(data) {
     document.getElementById("results-content").innerHTML = results;
     resultsModal.show();
 
+};
+
+function processOptions(form) {
+
+    let optArray = [];
+
+    for (let entry of form.entries()) {
+        if (entry[0] === "options") {
+            optArray.push(entry[1]);
+        };
+    };
+
+    form.delete("options");
+
+    form.append("options", optArray.join());
+
+    return form;
 }
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data.expiry);
+    } else {
+        displayException(data)
+        throw new Error(data.error);
+    };
+};
+
+async function postForm(e) {
+    const form = processOptions(new FormData(document.getElementById("checksform")));
+     
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayErrors(data);
+    }
+    else {
+        displayException(data)
+        throw new Error(data.error);
+    };
+};
+
+function displayException(data) {
+
+    let errorData = `<p>The API returned status code ${data.status_code}<br>
+    Error Number: <span class="fw-bold">${data.error_no}</span><br>
+    Error Text: <span class="fw-bold">${data.error}</span></p>`;
+
+    document.getElementById("resultsModalTitle").textContent = "An Exception Occurred";
+    document.getElementById("results-content").innerHTML = errorData;
+    resultsModal.show();
+};
 
